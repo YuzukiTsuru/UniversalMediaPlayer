9c65aabe19d3537fd0f78c46fd51e82f710d5c72
yuzukitsuru <740291272@qq.com>
2020-12-16 00:40:01 +0800

    add basic http server

---
+++ src/HTTPServer/HTTPServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ghost Gloomy on 2020/12/16.
+//
+
+#include "HTTPServer.h"
---
+++ src/HTTPServer/HTTPServer.h
@@ -0,0 +1,15 @@
+//
+// Created by Ghost Gloomy on 2020/12/16.
+//
+
+#ifndef UNIVERSALMEDIAPLAYER_HTTPSERVER_H
+#define UNIVERSALMEDIAPLAYER_HTTPSERVER_H
+#include <iostream>
+#include <mongoose.h>
+
+class HTTPServer {
+
+};
+
+
+#endif //UNIVERSALMEDIAPLAYER_HTTPSERVER_H
